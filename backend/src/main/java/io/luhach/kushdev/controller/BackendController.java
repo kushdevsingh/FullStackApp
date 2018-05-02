384603c889e6148ca40a2ab40501b9cce9b53163
Kushdev Singh Luhach <kushdev@gmail.com>
2018-05-02 16:02:18 -0400

    BackendController

--- backend/src/main/java/io/luhach/kushdev/controller/BackendController.java
+++ backend/src/main/java/io/luhach/kushdev/controller/BackendController.java
@@ -3,7 +3,6 @@ package io.luhach.kushdev.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
